9e9e1b70536b479195080ccee0fa2d1f84659b03
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-14 20:08:02 +0300

    Proje dosyası ekle.

---
+++ PatikaWeek5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> guest =new List<string>();//Yeni bir liste tanımlamak 
+
+Console.WriteLine("Kaç davetli geleceğini girermisiniz ");
+int guestnumber=Convert.ToInt32(Console.ReadLine());//Davetli sayısı girilerek döngünün kaç kez döneceği belirlenir 
+
+for(int i=0; i<guestnumber; i++)//Davetli girişi 
+{
+    Console.WriteLine("Lütfen davetli giriniz");
+    guest.Add(Console.ReadLine());
+    Console.WriteLine("------0----------0--------0------");
+}
+Console.WriteLine("Davetli sayınızın tam listesi");
+ 
+for (int i=0;i<guestnumber; i++)//Davetlileri okuma işlemi
+{
+    Console.WriteLine($"{i+1}) {guest[i]}");
+}
